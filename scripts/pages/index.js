1bb710dd716c5fe65350eed3542105d93c5a3b7a
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2025-07-12 15:42:39 +0200

    setup ok- data fetched

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,38 +1,56 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+
+              const response = await fetch("data/photographers.json");
+        const data = await response.json();
+        console.log(data[0])
+        return data;
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        // let photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Délia super Star",
+        //         "id": 3,
+        //         "city": "Poissy",
+        //         "country": "France",
+        //         "tagline": "Lovely data 3",
+        //         "price": 1500,
+        //         "portrait": "account.png"
+        //     },
+
+        // ]
+
+
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // return ({
+        //     photographers: [...photographers, ...photographers, ...photographers]})
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
+        photographers.forEach((photographer, index) => {
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
+            console.log(photographer.name, ",", photographer.price)
         });
     }
 
@@ -40,7 +58,7 @@
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+        console.log("j'attend que toute la data charge")
     }
-    
+
     init();
-    
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,29 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `../assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        // img.setAttribute("src", picture)
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const pCountry = document.createElement( 'p' );
+        pCountry.textContent = country;
+        const pTagline = document.createElement( 'p' );
+        pTagline.textContent = tagline;
+        const pPrice = document.createElement( 'p' );
+        pPrice.textContent = price;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pCountry);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, country, tagline, price, getUserCardDOM }
 }
